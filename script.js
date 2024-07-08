23ffc1419f528e8d5a82f997e7cbe49053c1bd3e
Aayusha <157830293+aayusha-03@users.noreply.github.com>
2024-07-08 20:12:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// Array of names
+let names = ["Yaakov" , "john" , "Jen" , "Jason" , "Paul" , "Frank" , "Larry" , "Paula" , "Laura" , "Jim"];
+// Loop over each name in the array
+for (let i = 0; i < names.length; i++) {
+  // Get the current name
+  let name = names[i];
+
+  // Check if the first letter is 'j' or 'J'
+  if (name[0].toLowerCase() === 'j') {
+    console.log("Goodbye " + name);
+  } else {
+    console.log("Hello " + name);
+  }
+}
